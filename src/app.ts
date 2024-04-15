f2a6d7a5d175b48d553b4db2ab550815bea9bc7c
manasdeo <rmnslogin@gmail.com>
2024-04-15 22:43:09 +0530

    middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,38 @@
+import dotenv from "dotenv";
+import "express-async-errors";
 import express, { Request, Response } from "express";
 
+// database
+import connectDB from "./db/connect";
+
+// error handler
+// import notFoundMiddleware from "./middleware/not-found";
+// import errorHandlerMiddleware from "./middleware/error-handler";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("<h1>File Upload Starter</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// middleware
+// app.use(notFoundMiddleware);
+// app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI as string);
+
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
