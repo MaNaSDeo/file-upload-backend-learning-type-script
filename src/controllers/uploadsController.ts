3a6fa66b3515a59d9f36282b7b3d59e7e587373a
manasdeo <rmnslogin@gmail.com>
2024-04-26 14:35:18 +0530

    upload image to cloudinary

--- src/controllers/uploadsController.ts
+++ src/controllers/uploadsController.ts
@@ -2,8 +2,9 @@ import { StatusCodes } from "http-status-codes";
 import { type Response, type Request } from "express";
 import path from "path";
 import CustomError from "../errors";
+import { v2 as cloudinary } from "cloudinary";
 
-const uploadProductImage = async (req: Request, res: Response) => {
+const uploadProductImageLocal = async (req: Request, res: Response) => {
   if (!req.files) {
     throw new CustomError.BadRequestError("No Files Uploaded");
   }
@@ -43,6 +44,42 @@ const uploadProductImage = async (req: Request, res: Response) => {
     } else {
       res.status(StatusCodes.BAD_REQUEST).send("Invalid image upload");
     }
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).send("No image uploaded");
+  }
+};
+
+const uploadProductImage = async (req: Request, res: Response) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No Files Uploaded");
+  }
+
+  if (req.files && req.files.image) {
+    const productImage = Array.isArray(req.files.image)
+      ? req.files.image[0]
+      : req.files.image;
+
+    if (!productImage.mimetype.startsWith("image")) {
+      throw new CustomError.BadRequestError("Please Upload Image");
+    }
+
+    const maxSize = Number(process.env.MAX_SIZE) || 1000000;
+    if (productImage.size > maxSize) {
+      throw new CustomError.BadRequestError(
+        `Please upload image smaller than ${maxSize / 1000}KB`
+      );
+    }
+
+    const result = await cloudinary.uploader.upload(productImage.tempFilePath, {
+      use_filename: true,
+      folder: "file-upload",
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      image: {
+        src: result.secure_url,
+      },
+    });
   } else {
     res.status(StatusCodes.BAD_REQUEST).send("No image uploaded");
   }
